87f6ea0b52fb1654a2b15ff65ed1eac4543713f0
BrunoPicolo <picolobruno@hotmail.com>
2019-03-23 18:02:43 +0100

    Ajout structure de la question 2 et module TYPE

--- TP32_PICOLO-ORTIZ_POUJOL.v
+++ TP32_PICOLO-ORTIZ_POUJOL.v
@@ -39,4 +39,56 @@ End Test.
 
 (* Implémentation de la spécification *)
 
+Module Type ALPHA.
+  Parameter lettre: Type.
+  Parameter eq: forall (x y:lettre), {x=y}+{x<>y}.
+End ALPHA.
+
+Module Type TYPE.
+  Parameter t: Type.
+End TYPE.
+
+Module Trie (A:ALPHA) (T:TYPE) <:
+  TABLE with Definition Key := list A.lettre with Definition Val := T.t.
+    Definition Key := list A.lettre.
+    Definition Val := T.t.
+    Inductive trie :=
+        Leaf (val: option Val)
+      | Node (val: option Val) (reste: A.lettre -> trie).
+    Definition table := trie.
+    Definition empty := Leaf None.
+    Definition isDefined T (v: option T) := (* teste si v <> None *)
+      match v with
+          None => false
+        | _ => true
+      end.
+    Definition getValue T (v: option T) def := (* valeur dans Some, def sinon *)
+      match v with
+          None => def
+        | Some u => u
+      end.
+
+    Fixpoint put t key val := à définir 
+    Fixpoint get t key val := à définir 
+    Fixpoint member t key := à définir
+
+    Theorem get_empty: forall key def, get empty key def = def.
+    Admitted.
+    Theorem get_put_eq: forall key val def t, get (put t key val) key def = val.
+    Admitted.
+    Theorem get_put_neq: forall key1 key2 val def t,
+    key1<>key2 -> get (put t key1 val) key2 def = get t key2 def.
+    Admitted.
+    Theorem empty_mem: forall key, member empty key = false.
+    Admitted.
+    Theorem mem_put_eq: forall key val t, member (put t key val) key = true.
+    Admitted.
+    Theorem mem_put_neq: forall key1 key2 val t,
+      key1<>key2 -> member (put t key1 val) key2 = member t key2.
+    Admitted.
+End trie.
+
+Inductive option T :=
+  Some (valeur:T)
+  | None.
 
